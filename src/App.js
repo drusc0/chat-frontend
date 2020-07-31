d5454882213b4e11b40261733287845b846e2294
Andres Rivero <andres.r90@gmail.com>
2020-07-31 11:37:43 -0500

    Initial commit with demo project working for chatting app.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { MessageList, MessageInput } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = new StreamChat('zffd8bmwg742');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFuY3ktdHJ1dGgtNiJ9.cYn47mIvyetDanzkDy6kxAUzgw5W2EFx9uCu4UqfaSw';
+
+chatClient.setUser(
+  {
+       id: 'fancy-truth-6',
+       name: 'Fancy truth',
+       image: 'https://getstream.io/random_png/?id=fancy-truth-6&name=Fancy+truth'
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'godevs', {
+  // add as many custom fields as you'd like
+  image: 'https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png',
+  name: 'Talk about Go',
+});
+
+const App = () => (
+  <Chat client={chatClient} theme={'messaging light'}>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
+
